bc4030ef39f5be6315028dc9f2627479f08108b4
abusalehrony <abusalehrony12@gmail.com>
2022-11-16 02:55:45 +0600

    added Navbar Correction

--- src/pages/shared/Navbar/Navbar.js
+++ src/pages/shared/Navbar/Navbar.js
@@ -10,24 +10,24 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/'>About</Link></li>
-                        <li><Link to='/'>Appointment</Link></li>
-                        <li><Link to='/'>Reviews</Link></li>
-                        <li><Link to='/'>Contact Us</Link></li>
-                        <li><Link to='/'>Login</Link></li>
+                        <li><Link to='/Home'>Home</Link></li>
+                        <li><Link to='/About'>About</Link></li>
+                        <li><Link to='/Appointment'>Appointment</Link></li>
+                        <li><Link to='/Reviews'>Reviews</Link></li>
+                        <li><Link to='/Contact'>Contact Us</Link></li>
+                        <li><Link to='/Login'>Login</Link></li>
                     </ul>
                 </div>
-                <Link to='/' className="btn btn-ghost normal-case text-xl">Doctors Portal</Link>
+                <Link to='/Home' className="btn btn-ghost normal-case text-xl">Doctors Portal</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/'>About</Link></li>
-                    <li><Link to='/'>Appointment</Link></li>
-                    <li><Link to='/'>Reviews</Link></li>
-                    <li><Link to='/'>Contact Us</Link></li>
-                    <li><Link to='/'>Login</Link></li>
+                    <li><Link to='/Home'>Home</Link></li>
+                    <li><Link to='/About'>About</Link></li>
+                    <li><Link to='/Appointment'>Appointment</Link></li>
+                    <li><Link to='/Reviews'>Reviews</Link></li>
+                    <li><Link to='/Contact'>Contact Us</Link></li>
+                    <li><Link to='/Login'>Login</Link></li>
                 </ul>
             </div>
 
