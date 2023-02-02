2e3e7f9a5168da41d5e1427302f722f66240aa61
abusalehrony <abusalehrony12@gmail.com>
2023-02-02 10:45:27 +0600

    added Footer

--- src/pages/Home/Home/Cardsection/Cardsection.js
+++ src/pages/Home/Home/Cardsection/Cardsection.js
@@ -8,7 +8,7 @@ const Cardsection = () => {
                 <img src={treatment} alt='' className="lg:max-w-[350px] sm:max-w-[120px] rounded shadow-2xl" />
                 <div className='lg:max-w-[450px] sm:max-w-[250px] lg: mx-6'>
                     <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
-                    <p className="py-6 ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                    <p className="py-6 ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                     <button className="btn btn-primary text-white">Get Started</button>
                 </div>
             </div>
--- src/pages/shared/Footer/Footer.js
+++ src/pages/shared/Footer/Footer.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import footer from '../../../assets/images/footer.png';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is Footer</h2>
+        <footer
+            style={{
+                background: `url(${footer})`,
+                backgroundSize: 'cover'
+            }}
+            className="p-10">
+            <div className='footer'>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <Link to="/" className="link link-hover">Emergency Checkup</Link>
+                    <Link to="/" className="link link-hover">Monthly Checkup</Link>
+                    <Link to="/" className="link link-hover">Weekly Checkup</Link>
+                    <Link to="/" className="link link-hover">Deep Checkup</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Oral Health</span>
+                    <Link to="/" className="link link-hover">Fluoride Treatment</Link>
+                    <Link to="/" className="link link-hover">Cavity Filling</Link>
+                    <Link to="/" className="link link-hover">Teath Whitening</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Our Address</span>
+                    <Link to="/" className="link link-hover">New York-101010 Hudson</Link>
 
-        </div>
+                </div>
+            </div>
+            <div className='text-center mt-32'>
+                <p>Copyright © 2023 - All right reserved</p>
+            </div>
+        </footer>
     );
 };
 
