851a24a3e6238825839b5fbb2dc05222ab43d5e7
abusalehrony <abusalehrony12@gmail.com>
2023-02-05 21:09:36 +0600

    added Appointment

---
+++ src/pages/Appointment/Appointment/Appointment.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AppointmentBanner from "../ApppintmentBanner/AppointmentBanner";
+
+
+const Appointment = () => {
+    return (
+        <div>
+            <AppointmentBanner></AppointmentBanner>
+        </div>
+    )
+}
+
+export default Appointment;
