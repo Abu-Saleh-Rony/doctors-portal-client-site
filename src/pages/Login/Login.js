9d1a954faaffae257b721f3c9f70e93217fa2f76
abusalehrony <abusalehrony12@gmail.com>
2023-03-07 17:22:21 +0600

    added Login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm();
+    const handleLogin = data => {
+        console.log(data);
+    }
     return (
-        <div>
-            <h2>This is login page</h2>
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-6'>
+                <h2 className='text-center'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="text" {...register("email")} className="input input-bordered w-full" />
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" {...register("password")} className="input input-bordered w-full" />
+                        <label className="label">
+                            <span className="label-text">Forget Password?</span>
+                        </label>
+                    </div>
+                    <input className='btn btn-accent w-full' value="Login" type="text" />
+
+                </form>
+            </div>
         </div>
     );
 };
